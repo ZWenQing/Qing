f6444f4b593dd4c476f635084a26a9ba8d191fc4
zwqing <471544661@qq.com>
2016-05-04 17:48:34 +0800

    myweb start

---
+++ qing/index.php
@@ -0,0 +1,13 @@
+<?PHP
+
+
+$expl = explode('/',$_GET['pel']);
+
+var_dump($expl);
+require_once('./qingphp/qingphp.php');
+
+
+
+
+
+?>
---
+++ qing/qingphp/qingphp.php
@@ -0,0 +1,16 @@
+<?PHP
+namespace qing\hel;
+
+class qing{
+  public $host;
+  
+  
+  public function index(){
+    echo '在'.__NAMESPACE__."里";
+    
+  }
+  
+  
+}
+$qing = new qing();
+?>
