ec99615f4ab098b4e88993031320f12eddcc78e6
zwqing <471544661@qq.com>
2016-05-08 21:38:24 +0800

    ..

--- qing/qingphp/qingphp.php
+++ qing/qingphp/qingphp.php
@@ -30,11 +30,12 @@ class controller{
       '/'.$Left.'\s+\$(\w*)\s+'.$Right.'/i',
       '/'.$Left.'\s+volist\s+\$([a-zA-Z][a-zA-Z0-9]*)\s+\$([a-zA-Z][a-zA-z0-9]*)\s+\$([a-zA-Z][a-zA-Z0-9]*)\s+'.$Right.'\s*(.*?)\s*'.$Left.'\/volist'.$Right.'/i',
     );
-    var_dump($pattern[1]);
+    //var_dump($pattern[1]);
     $replacement = array(
-      '<?php echo $this->arr[ "${1}" ] ?>',
+      '<?php echo (\$${1}) ; ?>',
       '<?php foreach( $this->arr["${1}"] as \$${2} => \$${3} ){ ?> ${4} <?php }?>'
     );
+    extract($this->arr,EXTR_OVERWRITE);
     $result = preg_replace( $pattern , $replacement , $content );
     file_put_contents( './../app/cache/index.html' , $result );
     include_once('./../app/cache/index.html');
