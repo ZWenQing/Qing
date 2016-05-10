a081ecc2f21c653adfbd9a79b2c61d232c6fe123
zwqing <471544661@qq.com>
2016-05-10 17:44:40 +0800

    add model

--- qing/index.php
+++ qing/index.php
@@ -2,11 +2,16 @@
 define('HOME','home');
 require './qingphp/mkfile.php';
 include './qingphp/qingphp.php';
-
-
+include './qingphp/model.class.php';
+use q\model;
+class models extends model{
+  
+}
+$model = new models();
 $pel = explode('/',$_GET['pel']);
 include_once( './../app/'.$pel[0].'/controller/'.$pel[1].'Controller.class.php' );
 $controller = '\home\index\\'.$pel[1].'Controller';
 $cont = new $controller;
 $cont->indexAction();
+
 ?>
