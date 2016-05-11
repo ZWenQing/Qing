c074530aaf92596d4b9c9705e1e6622e4f72e7a8
zwqing <471544661@qq.com>
2016-05-11 17:48:21 +0800

    pdo bug

--- qing/index.php
+++ qing/index.php
@@ -8,6 +8,7 @@ class userModel extends model{
   
 }
 $model = new userModel();
+$model->add(array("name"=>"abc"));
 $pel = explode('/',$_GET['pel']);
 include_once( './../app/'.$pel[0].'/controller/'.$pel[1].'Controller.class.php' );
 $controller = '\home\index\\'.$pel[1].'Controller';
--- qing/qingphp/model.class.php
+++ qing/qingphp/model.class.php
@@ -2,19 +2,31 @@
 namespace q;
 
 class model{
+  public $table_name = "";
+  public $pdo = null;
+  public $sql = array();
   public function __construct(){
     $modelname = str_replace("Model","",get_class($this));
-    $this->db($modelname);
+    $this->sql["table"] = $modelname;
+    $this->db();
     
   }
-  public function db($modelname){
+  public function db(){
     $config = include ("./qingphp/config/config.php");
     if( $config["db_type"] === "mysql" ){
       $connect = "mysql:host=".$config["host"].";dbname=".$config["db_name"];
-      // var_dump($connect);
-      $pdo = new \PDO($connect,$config["db_user"],$config["db_pwd"]) or die("未连接");
-      
+      $this->pdo = new \PDO($connect,$config["db_user"],$config["db_pwd"]) or die("连接失败");
+      $this->pdo->exec("SET CHARACTER SET utf8");
     }
+    return $this;
+  }
+  
+  public function add($data){
+    $sql = 'INSERT INTO '.$this->sql["table"]." VALUES ? ";
+    $this->pdo->prepare($sql);
+    $this->execute($data);
+    $res = $this->pdo->fetchAll();
+    var_dump();
   }
 }
 
