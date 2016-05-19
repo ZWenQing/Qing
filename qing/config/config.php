2a6b10fc51eeca29ba5fc2bfb8f2d82b3b09537b
zwqing <471544661@qq.com>
2016-05-19 23:24:38 +0800

    调整config

---
+++ qing/config/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  "db_type" => "mysql",
+  "host" => "localhost",
+  "db_name" => "test",
+  "db_user" => "root",
+  "db_pwd" => "root",
+  "db_port" => "3306"
+);
+?>
---
+++ qing/config/indexController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace home\index;
+use q\controller;
+class indexController extends controller{
+  public function indexAction(){
+    echo "<span style=\"font-size:40px;font-style:normal;font-weight:500px;\" >:)</span>";
+  }
+}
+
+?>
